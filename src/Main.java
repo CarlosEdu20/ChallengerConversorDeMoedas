9f95d9cf4a0d152edfc2fe89a9e4a0177737cd29
CarlosEdu20 <eduardocarlos3134@gmail.com>
2024-11-25 18:35:28 -0300

    Iniciando o projeto do conversor de moedas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int opcao = 0;
+        System.out.println("*********************************************************");
+        System.out.println("Seja bem vindo/a ao Conversor de Moeda =]");
+        System.out.println("          ");
+        System.out.println("1) Dolár =>> Peso argentino");
+        System.out.println("2) Peso argentino =>> Dólar");
+        System.out.println("3) Dolár =>> Real brasileiro");
+        System.out.println("4) Real brasileiro =>> Dólar");
+        System.out.println("5) Dolár =>> Peso colombiano");
+        System.out.println("6) Peso colombiano =>> Dolár");
+        System.out.println("7) Sair");
+        System.out.println("Escolha uma opção de moedas");
+        System.out.println("*********************************************************");
+        System.out.println("         ");
+        opcao = ler.nextInt();
+
+
 
 
     }
